873e4cc473a5ab7f3fc79b36fc3bcbfb3bd50a15
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-04-29 15:38:46 +0200

    adding tabs

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -2,7 +2,38 @@ export interface IconProps {
   kind: "chevron" | "menu" | "src";
 }
 
+type PlanetData = {
+  name: string;
+  overview: {
+    content: string;
+    source: string;
+  };
+  structure: {
+    content: string;
+    source: string;
+  };
+  geology: {
+    content: string;
+    source: string;
+  };
+  rotation: string;
+  revolution: string;
+  radius: string;
+  temperature: string;
+  images: {
+    planet: string;
+    internal: string;
+    geology: string;
+  };
+};
+
 export interface NavLinkProps {
-  name: string
-  borderColor: string
+  name: string;
+  borderColor: string;
+}
+
+export interface specializedViews {
+  planet: PlanetData;
+  activeColor: string;
+  colorTab: string;
 }
