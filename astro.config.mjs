0fb5ed01d178b3017796c27e14eac4e825e481a5
Calcagno Loïc <98493858+CalcagnoLoic@users.noreply.github.com>
2024-05-01 14:24:45 +0200

    fix configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: "https://calcagnoloic.github.io",
   base: "/planets-fact/",
   output: "server",
   adapter: netlify(),
